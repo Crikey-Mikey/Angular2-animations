27009af330bab1f53ee9fa9b00bc441219839071
Michael Lewis <mick.lewis84@gmail.com>
2016-12-15 23:15:05 +1100

    Add Shrink and translatex animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,70 @@
-import { Component } from '@angular/core';
+import { 
+	Component, 
+	trigger,
+	state,
+	style,
+	transition,
+	animate 
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+  	trigger('divState', [
+  		state('normal', style({
+  			'background-color': 'red',
+  			transform: 'translateX(0)'
+  		})),
+  		state('highlighted', style({
+  			'background-color': 'blue',
+  			transform: 'translateX(100px)'
+  		})),
+  		transition('normal <=> highlighted', animate(300)),
+  		// transition('highlighted => normal', animate(800))
+	]),
+	trigger('wildState', [
+  		state('normal', style({
+  			'background-color': 'red',
+  			transform: 'translateX(0) scale(1)'
+  		})),
+  		state('highlighted', style({
+  			'background-color': 'blue',
+  			transform: 'translateX(100px) scale(1)'
+  		})),
+  		state('shrunken', style({
+  			'background-color': 'green',
+  			transform: 'translateX(0) scale(0.5)'
+  		})),
+  		transition('normal => highlighted', animate(300)),
+  		transition('highlighted => normal', animate(800)),
+  		transition('shrunken <=> *', animate(500))
+	])
+  ] 
 })
 export class AppComponent {
- 
+	state = 'normal';
+	wildState = 'normal';
+	list = ['Milk', 'Bread', 'Sugar'];
+
+
+	onAnimate() {
+		this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+		this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+	}
+
+	onShrink() {
+		this.wildState = 'shrunken';
+	}
+
+	onAdd(item) {
+		this.list.push(item);
+	}
+
+	onDelete(item) {
+		this.list.splice(this.list.indexOf(item), 1);
+	}
+
+
+
 }
