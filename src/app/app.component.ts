6247557ced29fead8d75226c6eb3327815fa8e5c
Michael Lewis <mick.lewis84@gmail.com>
2016-12-18 20:44:33 +1100

    Add list animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { 
-	Component, 
+import {
+	Component,
 	trigger,
 	state,
 	style,
 	transition,
-	animate 
+	animate,
+	keyframes,
+	group
 } from '@angular/core';
 
 @Component({
@@ -47,8 +49,63 @@ import {
   			})),
   			animate(500)
 		])
+	]),
+	trigger('list1', [
+  		state('in', style({
+  			opacity: 1,
+  			transform: 'translateX(0)'
+  		})),
+  		transition('void => *', [
+			style({
+				opacity: 0,
+				transform: 'translateX(-100px)'
+			}),
+			animate(300)
+		]),
+		transition('* => void', [
+			animate(300, style({
+				transform: 'translateX(100px)',
+				opacity: 0
+			}))
+		])
+	]),
+	trigger('list2', [
+  		state('in', style({
+  			opacity: 1,
+  			transform: 'translateX(0)'
+  		})),
+  		transition('void => *', [
+  			animate(1000, keyframes([
+  				style({
+  					transform: 'translateX(-100px)',
+  					opacity: 0,
+            offset:0
+  				}),
+  				style({
+  					transform: 'translateX(-50px)',
+  					opacity: 0.5,
+            offset:0.3
+  				}),
+  				style({
+  					transform: 'translateX(-20px)',
+  					opacity: 1,
+            offset: 0.8
+  				}),
+          style({
+            transform: 'translateX(0)',
+            opacity: 1,
+            offset: 1
+          })
+			]))
+		]),
+		transition('* => void', [
+			animate(300, style({
+				transform: 'translateX(100px)',
+				opacity: 0
+			}))
+		])
 	])
-  ] 
+  ]
 })
 export class AppComponent {
 	state = 'normal';
@@ -71,6 +128,14 @@ export class AppComponent {
 
 	onDelete(item) {
 		this.list.splice(this.list.indexOf(item), 1);
+	}
+
+	animationStarted(event) {
+		console.log(event);
+	}
+
+	animationEnded(event) {
+		console.log(event);
 	}
 
 
